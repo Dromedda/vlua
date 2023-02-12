55224bd0cdba01001d7c05d9966aee65eff84685
Dromedda <erikgustafsson2001@gmail.com>
2023-02-12 22:39:26 +0100

    Merged Main into master

---
+++ build/test.lua
@@ -0,0 +1,1 @@
+local scene_test = {};;scene_test.init = function();	local a = 2;	if a ~= 1 then ;		print("A IS NOT 1");	end;	print("HI MOM!");end;;function scream() ;	print("AHHHHHHHHHHHHHHHHHHHHHHHHHHHHH");end;;scene_test.init();scream();return scene_test;
